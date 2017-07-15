f454dcd575ba545076252890b46bc9780f4f0306
Darnell Gillead <dlgillead@gmail.com>
2017-07-14 20:13:46 -0500

    added register route and view

--- controllers/home_controller.rb
+++ controllers/home_controller.rb
@@ -2,4 +2,8 @@ class App
   get '/' do
     erb :'home/index'
   end
+
+  get '/register' do
+    erb :'home/register'
+  end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -6,7 +6,19 @@ describe "home controller" do
     it "shows a welcome message" do
       get "/"
 
-      expect(last_response.body).to include("Welcome!")
+      expect(last_response.body).to include('Login')
+      expect(last_response.body).to include('Register')
+    end
+
+    it 'displays a form for the user to enter their registration information' do
+      get '/register'
+
+      expect(last_response.body).to include('<form')
+      expect(last_response.body).to include('</form>')
+      expect(last_response.body).to include('first_name')
+      expect(last_response.body).to include('last_name')
+      expect(last_response.body).to include('password')
+      expect(last_response.body).to include('email')
     end
   end
 end
